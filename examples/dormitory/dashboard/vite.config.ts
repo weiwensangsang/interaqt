5ef24c5251417c3502e2333194b9c1cf454f4dfd
xiazhen <weiwensangsang@users.noreply.github.com>
2025-07-31 17:39:29 +0800

    Update vite.config.ts

--- examples/dormitory/dashboard/vite.config.ts
+++ examples/dormitory/dashboard/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export default defineConfig({
     server: {
         host: true,
-        allowedHosts: 'all'
+        allowedHosts: true
     },
     esbuild: {
         jsxFactory: 'createElement',
