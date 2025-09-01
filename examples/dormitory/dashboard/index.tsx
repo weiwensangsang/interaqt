d1cd7c882c1d55ed857f6669bbf045a0c01ec1d9
xiazhen <weiwensangsang@users.noreply.github.com>
2025-09-01 14:04:40 +0800

    Revert "Update index.tsx"
    This reverts commit 4e5a0de82b442d11c4d2eb1b5bb9ced9149bf16c.

--- examples/dormitory/dashboard/index.tsx
+++ examples/dormitory/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import {createRoot, createElement} from "axii";
 import { Dashboard } from './Dashboard';
+import { entities, relations, interactions } from '../backend';
 
-createRoot(document.getElementById('root')!).render(<Dashboard entities={[]} relations={[]} interactions={[]}/>)
+createRoot(document.getElementById('root')!).render(<Dashboard entities={entities} relations={relations} interactions={interactions}/>)
