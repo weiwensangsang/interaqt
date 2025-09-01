4e5a0de82b442d11c4d2eb1b5bb9ced9149bf16c
xiazhen <weiwensangsang@users.noreply.github.com>
2025-09-01 13:57:48 +0800

    Update index.tsx

--- examples/dormitory/dashboard/index.tsx
+++ examples/dormitory/dashboard/index.tsx
@@ -1,5 +1,4 @@
 import {createRoot, createElement} from "axii";
 import { Dashboard } from './Dashboard';
-import { entities, relations, interactions } from '../backend';
 
-createRoot(document.getElementById('root')!).render(<Dashboard entities={entities} relations={relations} interactions={interactions}/>)
+createRoot(document.getElementById('root')!).render(<Dashboard entities={[]} relations={[]} interactions={[]}/>)
